8b51e7975e39f8f75764547dadbeecb5ce4b46fa
adrianvilla1504 <105081872+AdrianVilla1504@users.noreply.github.com>
2024-09-18 06:15:38 -0500

    feature: add movie mapper class and create mapper directory, add movie db response interface, map response from api

--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -1,11 +1,14 @@
 import { movieApi } from "@/core/api/movie-api"
 import { MovieDBMoviesResponse } from "@/infraestructure/interfaces/moviedb-response";
+import { MovieMapper } from "@/infraestructure/mappers/movie.mappers";
 
 export const nowPlayingAction = async() => {
     try{
         const {data} = await movieApi.get<MovieDBMoviesResponse>('/now_playing');
         console.log("data => ", JSON.stringify(data, null, 2));
-        return [];
+        const movies = data.results.map( MovieMapper.fromTheMovieDBToMovie)
+        console.log(movies);
+        return movies;
     }catch(error){
         console.error(error)
     }
--- infraestructure/interfaces/moviedb-response.ts
+++ infraestructure/interfaces/moviedb-response.ts
@@ -1,9 +1,22 @@
 export interface MovieDBMoviesResponse {
+    dates:         Dates;
+    page:          number;
+    results:       Result[];
+    total_pages:   number;
+    total_results: number;
+}
+
+export interface Dates {
+    maximum: Date;
+    minimum: Date;
+}
+
+export interface Result {
     adult:             boolean;
     backdrop_path:     string;
     genre_ids:         number[];
     id:                number;
-    original_language: string;
+    original_language: OriginalLanguage;
     original_title:    string;
     overview:          string;
     popularity:        number;
@@ -13,5 +26,12 @@ export interface MovieDBMoviesResponse {
     video:             boolean;
     vote_average:      number;
     vote_count:        number;
+}
+
+export enum OriginalLanguage {
+    CN = "cn",
+    En = "en",
+    Hi = "hi",
+    Zh = "zh",
 }
 
