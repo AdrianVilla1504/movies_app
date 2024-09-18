427de46a5517c1918b6f88baa009ef252a96dbe1
adrianvilla1504 <105081872+AdrianVilla1504@users.noreply.github.com>
2024-09-18 06:32:44 -0500

    feature: add tanstack query, and provider, create presentation hooks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import "../global.css"
-import { nowPlayingAction } from '@/core/actions/movies/now-playing.action';
+import { View, Text } from "react-native";
+import React from "react";
+import "../global.css";
+import { nowPlayingAction } from "@/core/actions/movies/now-playing.action";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const RootLayout = () => {
   nowPlayingAction();
   return (
-    <View>
-      <Text className='text-9xl'>LAYOUT</Text>
-    </View>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <View>
+        <Text className="text-9xl">LAYOUT</Text>
+      </View>
+    </QueryClientProvider>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
